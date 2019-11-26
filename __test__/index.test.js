1e3ef01a77b9a7cfbe34c3b48af5f87cc4fa36d6
narmander <narminshahin@gmail.com>
2019-11-26 13:32:51 -0500

    error not working

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,24 @@
 const { multipliesByFive, multipliesByFiveSlowly } = require("../index.js");
 
 describe("multipliesByFive", () => {
+    it('takes a number and multiplies it by 5', () => {
+        const testNum = 5;
+        const fiveTimedProduct = 25;
+
+        const product = multipliesByFive(testNum);
+
+        expect(product).toBe(fiveTimedProduct);
+    })
+
+    it('will throw an error if invalid input passed', () => {
+        const invalidInput = 'abc';
+        const myError = new Error('Pass numbers only')
+
+        const myFunction = multipliesByFive(invalidInput);
+
+
+        expect(myFunction).toThrow(myError);
+    })
 
 });
 
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function multipliesByFive(num) {
-
+  if(typeof num != 'number') {
+    throw new Error('Pass numbers only');
+  }
+	const result = num * 5;
+	return result;
 }
 
-function multipliesByFiveSlowly(num) {
-}
+function multipliesByFiveSlowly(num) {}
 
 module.exports = {
-  multipliesByFive,
-  multipliesByFiveSlowly
+	multipliesByFive,
+	multipliesByFiveSlowly,
 };
