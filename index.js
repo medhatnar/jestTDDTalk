4c87a2074efc70c1a59de43ba935166507057599
narmander <narminshahin@gmail.com>
2019-11-25 23:34:25 -0500

    less than 5

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+const { numberPrinter } = require('./printResults.js');
+
 function multipliesByFive(num) {
-  if (typeof num !== "number") {
-    throw new Error("Please provide a number!");
-  }
-  return num * 5;
+	if (typeof num !== 'number') {
+		throw new Error('Please provide a number!');
+	}
+	return num * 5;
 }
 
 function multipliesByFiveSlowly(num) {
-  return new Promise((resolve, reject) => {
-    if (typeof num !== "number") reject(num);
-    setTimeout(() => {
-      const result = num * 5;
-      resolve(result);
-    }, 500);
-  }).catch(error => {
-    throw new Error('Please give an input');
-  });
+	return new Promise((resolve, reject) => {
+		if (typeof num !== 'number') reject(num);
+		setTimeout(() => {
+			const result = num * 5;
+			resolve(result);
+		}, 500);
+	}).catch(error => {
+		throw new Error('Please give an input');
+	});
+}
+
+function printMultipliedByFive(num) {
+	numberPrinter(multipliesByFive(num));
 }
 
 module.exports = {
-  multipliesByFive,
-  multipliesByFiveSlowly
+	multipliesByFive,
+	multipliesByFiveSlowly,
+	printMultipliedByFive,
 };
--- printResults.js
+++ printResults.js
@@ -2,8 +2,8 @@ function numberPrinter(num) {
 	if (num < 5) {
 		console.log(num);
 	} else {
-        throw new Error('Only numbers lesser than 5 please');
-    }
+		console.warn('Only numbers lesser than 5 please');
+	}
 }
 
 module.exports = {
