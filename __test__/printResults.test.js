41005e07cbd1b3c9b052f49c1d3dc8da49632e12
narmander <narminshahin@gmail.com>
2019-11-26 16:18:47 -0500

    breaking integration test

--- __test__/printResults.test.js
+++ __test__/printResults.test.js
@@ -1,13 +1,12 @@
 const { numberPrinter } = require('../printResults.js');
 
-jest.spyOn(console, 'log');
-
 describe('numberPrinter', () => {
 	test('prints number less than 5 because it is lazy', () => {
-		const invalidNum = 4;
+		const myMockConsole = jest.spyOn(console, 'log');
+		const num = 4;
 
-		numberPrinter(invalidNum);
+		numberPrinter(num);
 
-		expect(console.log).toHaveBeenCalledWith(4);
+		expect(myMockConsole).toHaveBeenCalledWith(num);
 	});
 });
--- index.test.js
+++ index.test.js
@@ -1,21 +1,17 @@
 var { multipliesByFive, multipliesByFiveSlowly } = require('./index.js');
 
 describe('multipliesByFive', () => {
-	test('multiplies a given number by 5', () => {
-    
-  });
+	test('multiplies a given number by 5', () => {});
 
-	test('throws error on invalid input', () => {
-
-  });
+	test('throws error on invalid input', () => {});
 });
 
 describe('multiplyByFiveSlowly', () => {
-	test('multiplies a given number by 5, slowly', () => {
+	test('multiplies a given number by 5, slowly', () => {});
 
-  });
+	test('throws error on empty input', () => {});
+});
 
-	test('throws error on empty input', () => {
-
-  });
+describe('printMultipliedByFive', () => {
+	it('print out the result of multipliesByFive', () => {});
 });
