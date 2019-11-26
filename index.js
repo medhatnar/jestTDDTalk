dcdf8364cfac22bb25ebf451acaa81aa98985091
narmander <narminshahin@gmail.com>
2019-11-25 23:37:40 -0500

    no tests no functions

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 function multipliesByFive(num) {
-  if (typeof num !== "number") {
-    throw new Error("Please provide a number!");
-  }
-  return num * 5;
+
 }
 
 function multipliesByFiveSlowly(num) {
-  return new Promise((resolve, reject) => {
-    if (typeof num !== "number") reject(num);
-    setTimeout(() => {
-      const result = num * 5;
-      resolve(result);
-    }, 500);
-  }).catch(error => {
-    throw new Error('Please give an input');
-  });
 }
 
 module.exports = {
